ff0ef3da69bf4a35370e99a24410d2ead107275f
Pablo Pastor Martín <alu0100890839@ull.edu.es>
2018-01-17 16:55:43 +0000

    Inicio de la parte de audio

---
+++ scripts/Audio/tiempo.py
@@ -0,0 +1,27 @@
+import http.client
+import urllib.parse
+import json
+
+
+
+class Tiempo_api:
+    api_key = "d2d0ade56c57389b60485410a176c228"
+
+    def textoClima(self,nombre):
+        conn = http.client.HTTPConnection("api.openweathermap.org")
+        params = urllib.parse.urlencode({
+            "q": nombre,
+            "lang": "ES",
+            "units": "metric",
+            "appid": Tiempo_api.api_key
+        })
+
+        conn.request("GET", "/data/2.5/weather/?" + params)
+        response = conn.getresponse()
+
+        respuesta = json.loads(response.read().decode('utf-8'))
+
+        print("El tiempo en " + nombre + " es: " + respuesta["weather"][0]["description"] + ", con una temperatura de " + str(respuesta["main"]["temp"]).partition(".")[0] + " grados")
+        
+    
+Tiempo_api().textoClima("Santa Cruz de Tenerife")
