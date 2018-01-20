460d58d81bf0fad360c4733983d5091827b4cf66
Pablo Pastor Martín <alu0100890839@ull.edu.es>
2018-01-20 22:23:29 +0000

    Borrado

--- scripts/Signals/region.py
+++
@@ -1,9 +0,0 @@
-class Region:
-
-    def __init__(self, croped_img, x, y, w, h):
-        
-        self.img = croped_img
-        self.x = x
-        self.y = y
-        self.w = w 
-        self.h = h
